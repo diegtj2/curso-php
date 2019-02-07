f285d4265f58bb0cf9106c4d97820fa9b8b9653b
Diego <diego.januario@duosystem.com.br>
2019-02-07 15:35:22 -0200

    adc html

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
 $msg = "olá mundo \n";
 echo $msg;
-echo "\n\n outra msg aqui!";
+echo "\n outra msg aqui!";
 
+$login = $_POST['login'];
+
+echo $login;
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Minha Página</title>
+	</head>
+	<body>
+		<meta http-equiv="content-type" content="text/html"; charset="utf-8" />
+		<br/><br/><br/>
+		<form action="index.php">
+			Login: <input type="text" name="login"></input>
+			<br/>
+			Senha: <input type="text" name="pass"></input>
+			<br/>
+			<input type="submit" name="confirma" value="Confirma"></input>
+
+		</form>
+
+	</body>
+</html>
